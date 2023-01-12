5118622f3398ab2c9805563475da9bb9a4a12c72
Nikola Stefanovski <nikoding32@gmail.com>
2023-01-12 20:08:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const express = require('express');
     bodyParser = require('body-parser');
     bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost:27017/BananaFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/BananaFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
