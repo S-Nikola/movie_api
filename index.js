4a0a68e7b08de0f0addea98fa193e3ecf8e97d94
Nikola Stefanovski <nikoding32@gmail.com>
2023-03-07 12:39:34 +0100

    added allowed local host origin

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com', 'https://banana-flix.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com', 'https://banana-flix.netlify.app', 'http://localhost:420'];
 
 app.use(cors({
   origin: (origin, callback) => {
